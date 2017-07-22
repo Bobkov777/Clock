282c7894a57a4e454db6bf3bec569d20ec54eff6
Bobkov777 <bobkovilya@mail.ru>
2017-07-22 17:06:46 +0300

    .

---
+++ Clock/Clock.swift
@@ -0,0 +1,15 @@
+//
+//  Clock.swift
+//  Clock
+//
+//  Created by Ilya on 22.07.17.
+//  Copyright © 2017 Your School. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    var currentTime: Date {
+        return Date()
+    }
+}
